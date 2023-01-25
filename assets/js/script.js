124c92e25369b38e3c255bcc9b710f3d659230af
wallacebps <wallacebps@hotmail.com>
2023-01-25 19:32:45 -0300

    Dark mode improved

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,14 @@ fetch('https://restcountries.com/v2/all')
   .catch(error => console.log(error));
 
 const button = document.getElementById("dark-mode-btn");
+const text = document.getElementById("dark-mode-text");
   button.addEventListener("click", () => {
     const body = document.querySelector("body");
     body.classList.toggle("dark-mode");
-});  
+    button.classList.toggle("dark-mode");
+    if (body.classList.contains("dark-mode")) {
+      text.textContent = " Light Mode";
+    } else {
+      text.textContent = " Dark Mode";
+    }
+});
